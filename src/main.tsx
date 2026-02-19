cbc3268e4aacc17ffd21cb1dad77ea25f3d4fa18
Kentaa Kakashi <168970257+Kentaakakashi@users.noreply.github.com>
2026-02-19 17:15:58 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { FocusProvider } from "./context/FocusContext";
+
+<FocusProvider>
+  <App />
+</FocusProvider>
+
 
 createRoot(document.getElementById("root")!).render(<App />);

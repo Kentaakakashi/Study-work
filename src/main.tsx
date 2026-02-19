db53deb24d63edabc8f5a18141c462ab947d87eb
Kentaa Kakashi <168970257+Kentaakakashi@users.noreply.github.com>
2026-02-19 17:34:07 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
+// ✅ THIS LINE IS WHY YOUR SITE LOOKS DEAD
+import "./index.css";
+
 import { FocusProvider } from "./context/FocusContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
